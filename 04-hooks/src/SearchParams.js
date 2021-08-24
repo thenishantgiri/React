2c615b55e6d3c8cc1f180e33ea07ffcadfd2685d
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-08-24 19:40:52 +0530

    Hooks

---
+++ 04-hooks/src/SearchParams.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+const ANIMALS = Object.freeze(["dog", "bird", "cat", "rabbit", "reptile"]);
+
+const SearchParams = () => {
+  // creating hooks
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            onChange={(event) => setLocation(event.target.value)}
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(event) => setAnimal(event.target.value)}
+            onBlur={(event) => setAnimal(event.target.value)}
+          >
+            <option /> {/** an empty option selected */}
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(event) => setBreed(event.target.value)}
+            onBlur={(event) => setBreed(event.target.value)}
+          >
+            <option />
+
+            {breeds.map((breed) => (
+              <option value={breed} key={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
