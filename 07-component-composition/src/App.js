eb8d4e742312da59d8d3bbbba7c0e29d6ecefa00
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-08-25 15:59:30 +0530

    Component Composition

---
+++ 07-component-composition/src/App.js
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
+};
+
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
